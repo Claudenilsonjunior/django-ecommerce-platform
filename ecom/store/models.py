27b751f256f09360957b13074bf25a7a694eaa04
Claudenilson Júnior <claudenilsonjunior2@gmail.com>
2024-09-13 20:26:43 -0300

    Update models.py
    Adding a field for the sale price and to know if the product is on sale

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -25,9 +25,11 @@ class Product(models.Model):
     
     name = models.CharField(max_length=100)
     description = models.TextField(default = '')
-    price = models.DecimalField(default = 0.00, max_digits=6, decimal_places=2)
+    price = models.DecimalField(default = 0, max_digits=6, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default = 1)
     image = models.ImageField(upload_to='uploads/products')
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     
     def __str__(self): 
         return self.name
