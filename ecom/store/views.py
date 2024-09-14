b2639ef4ac19818a794b0aece22b60d9d226e39f
Claudenilson Júnior <claudenilsonjunior2@gmail.com>
2024-09-13 23:19:25 -0300

    Update views.py

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegistrationForm
 from django import forms
+
+
+
+
+def category(request, foo):
+    # Replace '-' with ' ' in the category name
+    foo = foo.replace('-', ' ')
+    # Grab the category from the url
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'category': category, 'products': products})
+    except:
+        messages.error(request, 'Category not found')
+        return redirect('home')
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
+
 
 def home(request):
     products = Product.objects.all()
