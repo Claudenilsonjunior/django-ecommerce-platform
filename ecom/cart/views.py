36014499052457665af56068c17ce316e12e0d99
Claudenilson Júnior <claudenilsonjunior2@gmail.com>
2024-09-15 18:56:28 -0300

    Creating some views for the cart

---
+++ ecom/cart/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+
+def cart_summary(request):
+    return render(request, 'cart_summary.html', {})
+
+def add_to_cart(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = request.POST.get('product_id')
+        product = get_object_or_404(Product, id=product_id)
+        cart.add_to_cart(product = product)
+        response = JsonResponse({'Product Name': product.name})
+        return response
+
+def delete_from_cart(request):
+    pass
+
+def update_cart(request): 
+    pass
