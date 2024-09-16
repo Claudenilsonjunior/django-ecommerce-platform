92d3c3409f71a52fd59746fbcdc61c00b6d538ed
Claudenilson Júnior <claudenilsonjunior2@gmail.com>
2024-09-16 19:53:45 -0300

    Update views.py

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -4,15 +4,25 @@ from store.models import Product
 from django.http import JsonResponse
 
 def cart_summary(request):
-    return render(request, 'cart_summary.html', {})
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    return render(request, 'cart_summary.html', {"cart_products": cart_products})
 
 def add_to_cart(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
+        
+        #Getting the Id of the product
         product_id = request.POST.get('product_id')
         product = get_object_or_404(Product, id=product_id)
         cart.add_to_cart(product = product)
-        response = JsonResponse({'Product Name': product.name})
+        
+        #Getting cart quantity
+        
+        cart_quantity = cart.__len__()
+        
+        #response = JsonResponse({'Product Name': product.name})
+        response = JsonResponse({'Quantity': cart_quantity})
         return response
 
 def delete_from_cart(request):
